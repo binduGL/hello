0da15901859014968f63e6fd789285b592f1a8ad
binduGL <135324522+binduGL@users.noreply.github.com>
2023-07-15 19:00:04 +0530

    Update sampledemo.py
    some modification

--- sampledemo.py
+++ sampledemo.py
@@ -1,5 +1,10 @@
-
+print("student information")
+print("="*50)
+id=int(input("enter ur student id:"))
 name=input("enter name:")
-print("*"*50)
-
-print("my name is:{}".format(name))
+marks=int(input("enter ur marks"))
+print("="*50)
+print("id:{}".format(id))
+print("name:{}".format(name))
+print("marks:{}".format(marks))
+print("="*50)
